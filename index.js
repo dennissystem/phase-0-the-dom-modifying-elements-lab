0bc7e19a958b0a5df978c6706aff6ecb0d353d37
Eren Kocak <eren.k.kocak@gmail.com>
2021-05-10 22:06:06 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id="victory";
+document.querySelector("h1#victory");
+mainWithMainId = document.querySelector("#main").remove();
